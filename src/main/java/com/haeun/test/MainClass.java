b247e1fff99b1d9ba92493c97e385740a38ee204
ATE <ATE@DESKTOP-ODIPDNU>
2022-07-07 12:50:00 +0900

    스프링 빈 설정 연습

---
+++ src/main/java/com/haeun/test/MainClass.java
@@ -0,0 +1,36 @@
+package com.haeun.test;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+	public static void main(String[] args) {
+		String config1 = "classpath:applicationCTX.xml";
+		String config2 = "classpath:applicationCTX2.xml";
+		//Container 파일 불러오기
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(config1,config2);
+		
+	 	Student student1 = ctx.getBean("student1", Student.class);
+		
+	 	StudentInfo studentInfo = ctx.getBean("studentInfo1", StudentInfo.class);
+		
+	 	System.out.println(student1.getName());
+	 	System.out.println(student1.getHobbys());
+	 	
+	 	Student stu1 = studentInfo.getStudent();
+	 	System.out.println(stu1.getName());
+	 	System.out.println(stu1.getHobbys());
+	 	
+	 	Student student2 = ctx.getBean("student2",Student.class);
+	 	System.out.println(student2.getName());
+	 	System.out.println(student2.getHobbys());
+	 	
+	 	Family family = ctx.getBean("family", Family.class);
+	 	System.out.println(family.getPapaName());
+	 	System.out.println(family.getMamiName());
+	 	System.out.println(family.getSisterName());
+	 	System.out.println(family.getBrotherName());
+	 	
+		ctx.close();
+	}
+}
---
+++ src/main/java/com/haeun/test/StudentInfo.java
@@ -0,0 +1,20 @@
+package com.haeun.test;
+
+public class StudentInfo {
+
+	private Student student;
+	
+	
+	public StudentInfo() {	//ºó »ı¼ºÀÚ
+		super();
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	
+}
