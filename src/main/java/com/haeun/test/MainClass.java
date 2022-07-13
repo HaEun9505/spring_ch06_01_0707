9d81d7145ec17667835fb73c43398ed69efe8182
ATE <ATE@DESKTOP-ODIPDNU>
2022-07-13 10:18:55 +0900

    스프링 빈 설정 연습(주석)

--- src/main/java/com/haeun/test/MainClass.java
+++ src/main/java/com/haeun/test/MainClass.java
@@ -10,22 +10,25 @@ public class MainClass {
 		//Container 파일 불러오기
 		AbstractApplicationContext ctx = new GenericXmlApplicationContext(config1,config2);
 		
-	 	Student student1 = ctx.getBean("student1", Student.class);
+		//applicationCTX.xml에서 설정한 값 가져오기(bean 조회)
+	 	Student student1 = ctx.getBean("student1", Student.class);	//bean 조회(객체 얻어오기)
 		
 	 	StudentInfo studentInfo = ctx.getBean("studentInfo1", StudentInfo.class);
 		
 	 	System.out.println(student1.getName());
 	 	System.out.println(student1.getHobbys());
 	 	
-	 	Student stu1 = studentInfo.getStudent();
+	 	Student stu1 = studentInfo.getStudent();	//StudentInfo클래스의 getStudent 호출(실행)
 	 	System.out.println(stu1.getName());
 	 	System.out.println(stu1.getHobbys());
 	 	
+	 	
+	 	//applicationCTX2.xml에서 설정한 값 가져오기(bean 조회)
 	 	Student student2 = ctx.getBean("student2",Student.class);
 	 	System.out.println(student2.getName());
 	 	System.out.println(student2.getHobbys());
 	 	
-	 	//applicationCTX2.xml에서 설정한 값 가져오기
+	 	
 	 	Family family = ctx.getBean("family", Family.class);
 	 	System.out.println(family.getPapaName());
 	 	System.out.println(family.getMamiName());
